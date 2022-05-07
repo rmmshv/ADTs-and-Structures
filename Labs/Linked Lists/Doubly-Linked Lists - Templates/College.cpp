c2f03e18f6ef4dc8401bc5f60feb97ed62f7393f
rimma <32555704+rmmshv@users.noreply.github.com>
2022-05-07 05:43:47 -0700

    To fix: searchList() in LinkedListADT.h doesn't work

---
+++ Labs/Linked Lists/Doubly-Linked Lists - Templates/College.cpp
@@ -0,0 +1,74 @@
+// Implementation file for the College class
+// Written By: A. Student
+// Changed By: Rimma Esheva
+// IDE: VS code
+
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+#include "College.h"
+
+using namespace std;
+
+//**************************************************
+// Constructor
+//**************************************************
+College::College()
+{
+    rank = -1;
+    code = "";
+    name = "";
+    cost = -1;
+}
+
+//**************************************************
+// Overloaded Constructor
+//**************************************************
+College::College(int rk, string cd, string nm, int ct)
+{
+    rank = rk;
+    code = cd;
+    name = nm;
+    cost = ct;
+}
+
+//***********************************************************
+// Displays the values of the College object member variables
+// on one line (horizontal display)
+//***********************************************************
+void College::hDdisplay() const
+{
+    cout << left;
+    cout << " " << setw(4) << code << "  ";
+    cout << " " << setw(2) << rank << "  ";
+    cout << " " << setw(27)<< name << "  ";
+    cout << right;
+    cout << " " << setw(7) << cost << " ";
+    cout << left << endl;
+}
+
+//***********************************************************
+// Displays the values of the College object member variables
+// one per line (vertical display)
+//***********************************************************
+void College::vDisplay() const
+{
+    cout << "              Rank: " << rank << endl;
+    cout << "       School Name: " << name << endl;
+    cout << "Cost of Attendance: $" << cost << endl;
+}
+
+// overloaded operators
+ostream& operator<<(ostream &out, const College &college) {
+    cout << left;
+    cout << " " << setw(4) << college.code << "  ";
+    cout << " " << setw(2) << college.rank << "  ";
+    cout << " " << setw(27)<< college.name << "  ";
+    cout << right;
+    cout << " " << setw(7) << college.cost << " ";
+    cout << left << endl;
+
+    return out;
+}
